0ba5cd15255ea879dc7d28a60e9b405fa1a8d6ee
Your Name <chrischo360@gmail.com>
2025-02-13 11:45:09 -0500

    feat: get boilerplate working

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,6 +1,39 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import '@mantine/core/styles.css';
+import type { AppProps } from 'next/app';
+import { AppShell, MantineProvider, Container } from '@mantine/core';
+import Head from 'next/head';
+import { theme } from '../../theme';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <MantineProvider theme={theme}>
+      <Head>
+        <title>LifeOS Client</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"
+        />
+        <link rel="shortcut icon" href="/favicon.svg" />
+      </Head>
+      <AppShell
+        header={{
+          height: 60,
+          collapsed: false,
+          offset: true
+        }}
+        footer={{ height: 60 }}
+        padding="md"
+      >
+        <AppShell.Header>
+          <></>
+        </AppShell.Header>
+        <AppShell.Main>
+          <Container size="sm" py="xl">
+            <Component {...pageProps} />
+          </Container>
+        </AppShell.Main>
+        <AppShell.Footer />
+      </AppShell>
+    </MantineProvider>
+  );
 }
---
+++ client/theme.ts
@@ -0,0 +1,48 @@
+import { createTheme, colorsTuple } from '@mantine/core';
+
+export const theme = createTheme({
+  defaultRadius: 'md',
+  cursorType: 'pointer',
+  colors: {
+    background: colorsTuple('#F9F4EF'),
+    headline: colorsTuple('#020826'),
+    paragraph: colorsTuple('#020826'),
+    button: colorsTuple('#8C7851'),
+    buttonText: colorsTuple('#FFFFFF')
+  },
+  fontFamily: 'Verdana, sans-serif',
+  fontFamilyMonospace: 'Monaco, Courier, monospace',
+  headings: {
+    fontFamily: 'Greycliff CF, sans-serif'
+  },
+  components: {
+    Button: {
+      defaultProps: {
+        color: 'button',
+        variant: 'outline'
+      },
+      styles: {
+        root: {
+          color: 'buttonText',
+          '&:hover': {
+            backgroundColor: 'inherit',
+            color: 'buttonText'
+          }
+        }
+      }
+    },
+    Text: {
+      defaultProps: {
+        color: 'paragraph'
+      }
+    },
+    Title: {
+      defaultProps: {
+        color: 'headline'
+      }
+    }
+  },
+
+  primaryColor: 'button',
+  primaryShade: 5
+});
