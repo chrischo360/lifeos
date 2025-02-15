9e002fd8210db72eec3b05379c1f0a8002584164
Your Name <chrischo360@gmail.com>
2025-02-15 08:44:20 -0500

    feat: add functionality to add goals and get goals from server

--- server/app.py
+++ server/app.py
@@ -1,28 +1,48 @@
-from chalice import Chalice
+from chalice import Chalice, Response
+from chalice import BadRequestError
+import uuid
+from chalice.app import CORSConfig
+
+# Create a CORS configuration
+cors_config = CORSConfig(
+    allow_origin='http://localhost:3000',  # Specify the exact client origin
+    allow_headers=['Content-Type'],
+    max_age=600,
+    expose_headers=['X-Special-Header'],
+    allow_credentials=True
+)
 
 app = Chalice(app_name='lifeos_server')
 
-@app.route('/')
+# In-memory storage for goals for now
+goals = []
+
+@app.route('/', methods=['GET'], cors=cors_config)
 def index():
     return {'hello': 'world'}
 
+@app.route('/goal', methods=['POST'], cors=cors_config)
+def create_goal():
+    request_body = app.current_request.json_body
+    
+    # Validate required fields
+    if not request_body.get('title'):
+        raise BadRequestError('Goal title is required')
+    
+    # Generate unique ID
+    goal_id = str(uuid.uuid4())
+    
+    new_goal = {
+        'id': goal_id,
+        'title': request_body.get('title'),
+        'description': request_body.get('description', ''),
+        'tags': request_body.get('tags', [])
+    }
+    
+    goals.append(new_goal)
+    
+    return Response(body=new_goal, status_code=201)
 
-# The view function above will return {"hello": "world"}
-# whenever you make an HTTP GET request to '/'.
-#
-# Here are a few more examples:
-#
-# @app.route('/hello/{name}')
-# def hello_name(name):
-#    # '/hello/james' -> {"hello": "james"}
-#    return {'hello': name}
-#
-# @app.route('/users', methods=['POST'])
-# def create_user():
-#     # This is the JSON body the user sent in their POST request.
-#     user_as_json = app.current_request.json_body
-#     # We'll echo the json body back to the user in a 'user' key.
-#     return {'user': user_as_json}
-#
-# See the README documentation for more examples.
-#
+@app.route('/goals', methods=['GET'], cors=cors_config)
+def list_goals():
+     return Response(body=goals, status_code=200)
